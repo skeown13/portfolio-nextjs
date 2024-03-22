486b6ebc3f6241d58a3d85a1400384d8fc0dc99e
skeown13 <code@keown.net>
2024-03-22 10:42:51 -0700

    added static data to be pulled from

---
+++ constants/index.ts
@@ -0,0 +1,137 @@
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    skill_name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    Image: "/redux.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "TypeScript",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next js 14",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Framer Motion",
+    Image: "/framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Stripe Payment",
+    Image: "/stripe.webp",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Mongo db",
+    Image: "/mongodb.png",
+    width: 40,
+    height: 40,
+  },
+];
+
+export const Socials = [
+  {
+    name: "Discord",
+    src: "/discord.svg",
+  },
+  {
+    name: "Facebook",
+    src: "/facebook.svg",
+  },
+  {
+    name: "Instagram",
+    src: "/instagram.svg",
+  },
+];
+
+export const Projects = [
+  {
+    title: "modern Nextjs Website",
+    text: "An animated website with the latest tech",
+    src: "/NextWebsite.png",
+  },
+  {
+    title: "modern Nextjs Website",
+    text: "An animated website with the latest tech",
+    src: "/NextWebsite.png",
+  },
+  {
+    title: "modern Nextjs Website",
+    text: "An animated website with the latest tech",
+    src: "/NextWebsite.png",
+  },
+  {
+    title: "modern Nextjs Website",
+    text: "An animated website with the latest tech",
+    src: "/NextWebsite.png",
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "my-skills",
+    icon: RxPerson,
+    link: "/my-skills",
+  },
+  {
+    name: "my-projects",
+    icon: RxDashboard,
+    link: "/my-projects",
+  },
+  {
+    name: "contact-me",
+    icon: RxClipboard,
+    link: "/contact-me",
+  },
+];
